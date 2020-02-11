888229985596d78dec9998decb35b24461e22117
archi777 <wowgladarena@mail.ru>
2020-02-11 12:24:21 +0300

    Updates

---
+++ precache-manifest.6ba9fc7318ead588abfd8829e8e03b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72691a748c5b97fbe4848bfd8abca271",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "8f9efd791df8756ab4f1",
+    "url": "/aviasales/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "8f9efd791df8756ab4f1",
+    "url": "/aviasales/static/js/2.b6165701.chunk.js"
+  },
+  {
+    "revision": "c76a8c75c1f9e4220a3bd81741d9248d",
+    "url": "/aviasales/static/js/2.b6165701.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82348b51c39efac82036",
+    "url": "/aviasales/static/js/main.a922c4ef.chunk.js"
+  },
+  {
+    "revision": "0b09bd41ba4f766bb396",
+    "url": "/aviasales/static/js/runtime-main.2d1eab4e.js"
+  }
+]);
